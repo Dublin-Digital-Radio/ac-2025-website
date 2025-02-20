d7a3df46ae7a6d117fdf7dd69618d0250cefc7ed
Tewson Seeoun <tewson.seeoun@gmail.com>
2025-02-20 21:50:50 +0000

    Add GitHub pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://dublin-digital-radio.github.io',
+  base: 'ac-2025-website-prototype-astro',
   vite: {
     plugins: [tailwindcss()]
   }
