6254dc569e542a991e12e3dac60006599751a1e4
Tewson Seeoun <tewson.seeoun@gmail.com>
2025-03-30 15:44:58 +0100

    Add zod

---
+++ src/api.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+const artistSchema = z.object({
+  name: z.string(),
+  description: z.string(),
+});
+
+export const artistListSchema = z.array(artistSchema);
+
+const venueSchema = z.object({
+  name: z.string(),
+});
+
+export const eventListSchema = z.array(
+  z.object({
+    name: z.string(),
+    artists: artistListSchema,
+    venue: venueSchema,
+  })
+);
