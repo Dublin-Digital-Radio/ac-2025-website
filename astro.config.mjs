938cae3b0b93f2fb4c1090ee86cee05d4d7289d3
Tewson Seeoun <tewson.seeoun@gmail.com>
2025-03-30 17:51:33 +0100

    Group events by day

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+
+const base = "ac-2025-website-prototype-astro";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dublin-digital-radio.github.io',
-  base: 'ac-2025-website-prototype-astro',
+  site: "https://dublin-digital-radio.github.io",
+  base,
+  redirects: {
+    "/schedule": `/${base}/schedule/2025-05-01`,
+  },
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
