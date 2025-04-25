b12f026f77a244c39ffe23f595fdaf6d60c787dc
Tewson Seeoun <tewson.seeoun@gmail.com>
2025-04-25 20:05:11 +0100

    Nicer schedule page redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,6 @@ const base = "alternating-current-2025";
 export default defineConfig({
   site: "https://listen.dublindigitalradio.com",
   base,
-  redirects: {
-    "/schedule": `/${base}/schedule/may-1`,
-  },
   vite: {
     plugins: [tailwindcss()],
   },
